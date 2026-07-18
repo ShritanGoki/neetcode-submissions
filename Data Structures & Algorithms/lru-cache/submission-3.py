36cc28213971644c318141249cfc77bb05641eaf
ShritanGoki <shritangoki0028@gmail.com>
2026-07-18 19:05:42 -0400

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, key, val, prev = None, next = None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} # key, node
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        else:
+            return -1
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            remove = self.left.next
+            self.remove(remove)
+            del self.cache[remove.key]
+
+        
+    
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.right
+        self.right.prev = node
+
+
+    def remove(self, node):
+        prev = node.prev
+        post = node.next
+        prev.next = post
+        post.prev = prev
+        
+        
